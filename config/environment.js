de7804b85e05a0e715fc835e7a925ac23142da57
5harath <19461590+5harath@users.noreply.github.com>
2023-04-30 14:33:22 +0530

    Add files

---
+++ config/environment.js
@@ -0,0 +1,12 @@
+const developement = {
+    name: 'developement',
+    assets_path: './assets',
+    db_path: './config/mongoose',
+    passport_local_path: './config/passport-local-strategy',
+    secret_key: "#placementCell@careercamp$$Team&&&interface##sharath^^J%%",
+    mongoose_connect:'mongodb+srv://admin:vrdq1qtkQWAYYrlG@cluster0.izlqkhy.mongodb.net/?retryWrites=true&w=majority'
+
+}
+
+
+module.exports = developement;
